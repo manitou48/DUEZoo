d7413d2553a44c43d4c36897674e9803b6bba672
manitou48 <manitou@home>
2013-03-29 07:03:11 -0400

    isrperf update

--- isrperf.ino
+++ isrperf.ino
@@ -3,40 +3,44 @@
 //  fast port write  (bit band)
 // output 13   input 2   B.25
 
-volatile unsigned int t2;
-unsigned int t1,t3;
+volatile unsigned int ti;
 
 void isr() {
-	t2 = SysTick->VAL;   // or do this in PIOB_Handler WIntterupts.c 
+	ti = SysTick->VAL;   // or do this in PIOB_Handler WIntterupts.c 
 } 
 
 void setup() {
+	unsigned int t1,t2,t3,t4,t5;
+	char str[96];
+
 	pinMode(13,OUTPUT);
 	g_APinDescription[13].pPort->PIO_SODR = g_APinDescription[13].ulPin;   // LED on
 	Serial.begin(9600);
+	delay(123);			// quiesce
 	t1 = SysTick->VAL;
-	t3 = SysTick->VAL;
-	Serial.println(t1-t3);
-	t1 = SysTick->VAL;
+	t2 = SysTick->VAL;
 	g_APinDescription[13].pPort->PIO_SODR = g_APinDescription[13].ulPin;
 	t3 = SysTick->VAL;
-	Serial.println(t1-t3);
-	t1 = SysTick->VAL;
 	digitalWrite(13,LOW);  // set bit
-	t3 = SysTick->VAL;
-	Serial.println(t1-t3);
+	t4 = SysTick->VAL;
+	isr();
+	t5 = SysTick->VAL;
+	sprintf(str,"tick %d  set %d  write %d  fcn %d %d",
+	  t1-t2,t2-t3,t3-t4, t4-ti,ti-t5);
+	Serial.println(str);
 	attachInterrupt(2,isr,RISING);
 }
 
 void loop(){
+	unsigned int t1,t3;
 	char str[64];
 
-	t2=0;
+	ti=0;
 	t1 = SysTick->VAL;
 	g_APinDescription[13].pPort->PIO_SODR = g_APinDescription[13].ulPin;   // LED on
-	while(t2 == 0);  // wait
+	while(ti == 0);  // wait
 	t3 = SysTick->VAL;
-	sprintf(str,"%d %d %d  %d %d  %d",t1,t2,t3,t1-t2,t2-t3,t1-t3);  
+	sprintf(str,"%d %d %d  isr %d %d",t1,ti,t3,t1-ti,ti-t3);  
 	Serial.println(str);
 	digitalWrite(13,LOW);
 	delay(3000);
